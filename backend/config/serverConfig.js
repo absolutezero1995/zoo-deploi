ada866780e904bcc1e204d56a3ed0663d75a1501
absolutezero1995 <sokolovmaksim2391@gmail.com>
2024-01-22 16:34:15 -0100

    monday2

--- backend/config/serverConfig.js
+++ backend/config/serverConfig.js
@@ -4,11 +4,13 @@ const cookieParser = require("cookie-parser");
 const ssr = require("../middleware/ssr");
 const getUser = require("../middleware/getUser");
 const sessionConfig = require("../config/sessionConfig");
+const cors = require("cors");
 
 
 
 
 const serverConfig = (app) => {
+  app.use(cors());
   app.use(cookieParser());
   app.use(session(sessionConfig));
   app.use(express.urlencoded({ extended: true }));
